b220a2203c983ca84d27fdcc10d3e31a45d086fc
YatinK <yatin.kare@gmail.com>
2026-06-27 16:49:31 -0400

    feat: write unchecked values to file.

--- README.md
+++ README.md
@@ -1,11 +1,41 @@
 # Golang Daily To-Do CLI
 
-A cli go daily to-do manager. Features include:
+A cli go daily to-do manager in ~100 lines. Features include:
 
 - Creating Daily Notes with a template to a directory
 - Migrating Un-checked to-do's from the previous day into today's daily note upon creation
 
 Motivation: Learning Golang syntax
+
+## Usage
+
+```bash
+go build to-do.go
+./to-do --version
+./to-do --help
+./to-do new
+```
+
+## Learnings
+
+I learned...
+- How to capture command line arguments with `os.Args`
+- How to do switch statements
+- The different ways of printing (`fmt.Println, fmt.Printf)
+- How to format a string with `fmt.Sprintf()`
+- How to declare a function that returns a value 
+- How to check for errors
+- How to use the `time` package to use dates
+- How to handle file paths with the `path/filepath` package.
+- How to read a file with `os.ReadFile`
+- How to convert bytes to strings
+- how to use the `strings` package to split strings, to check if a string contains a substring
+- How to initialize a slice (not an array)
+- How to itterate over a slice using the `_, val := range {slice}` syntax
+- How to append to a slice
+- How to write buffers
+- How to open a file, use file descriptors, check if files exists and write a buffer to a file.
+- How to handle errors gracefully
 
 ---
 > Author: Yatin Kare
---
+++ to-do.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+	"strings"
+)
+
+func main() {
+	var args = os.Args
+	if len(args) > 1 {
+		switch args[1] {
+			case "--version":
+				fmt.Println("1.0.0")
+			case "--help":
+				fmt.Println("./to-do new: creates new todo with left over todos from the previous day")
+			case "new":
+				unchecked_move_to_tommorrow := checkPrevDailyNote()
+				if err := write_new_file(unchecked_move_to_tommorrow); err != nil {
+					fmt.Println("[ERROR]:", err)
+				}
+			default:
+				fmt.Println("I don't know that command, sorry")
+		}
+	}
+}
+
+func checkPrevDailyNote() []string {
+	curr_dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	now := time.Now()
+	mon, day, yer := now.Month(), now.Day(), now.Year()
+	today_file := fmt.Sprintf("%04d-%02d-%02d.md", yer, mon, day)
+	today_filepath := filepath.Join(curr_dir, today_file)
+
+	today_bytedata, err := os.ReadFile(today_filepath)
+	today_data := string(today_bytedata)
+
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(today_data, "\n")
+
+	// loop through all lines of today's file
+	var unchecked []string
+
+	for _, val := range lines {
+		if strings.Contains(val, "- [ ]") {
+			// start := strings.Index(val, "- [ ]")
+			// fmt.Printf("FOUND UNCHECKED [%d]:%s\n", index, val)
+			// fmt.Printf("start %d\n", start)
+
+			// unchecked_value := val[start + 6:len(val)]
+			unchecked = append(unchecked, val)
+		}
+	}
+	return unchecked
+}
+
+func write_new_file(unchecked_values []string) error {
+	curr_dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	now := time.Now().AddDate(0, 0, 1)
+	mon, day, yer := now.Month(), now.Day(), now.Year()
+	tomorrow_file := fmt.Sprintf("%04d-%02d-%02d.md", yer, mon, day)
+	tomorrow_filepath := filepath.Join(curr_dir, tomorrow_file)
+
+	// buffer to write to file
+	var buff string
+	buff = buff + "# " + tomorrow_file[:len(tomorrow_file) - 3] + "\n"
+	for _, unchecked_val := range unchecked_values {
+		buff = buff + unchecked_val + "\n"
+	}
+
+	// os.CREATE: creates if does not exist, otherwise opens the existing one
+	// os.EXCL: **only when os.CREATE is set**: returns err if there is an existing one
+	f, err := os.OpenFile(tomorrow_filepath, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0666)
+	if err != nil {
+		if os.IsExist(err) {
+			return fmt.Errorf("File exists in the current director, please check: \n%s\n", tomorrow_filepath)
+		}
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(buff)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
